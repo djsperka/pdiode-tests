eae9d5154a868b9391bd5c95fa0950d5cfb07461
unknown <Lab@cns-cd2jcv6.ou.ad3.ucdavis.edu>
2024-07-10 13:11:58 -0700

    update

--- flash-long.bat
+++ flash-long.bat
@@ -9,4 +9,4 @@ REM st = index (0-based) of starting term in sequence (-e arg)
 REM nt = number of terms in sequence (-e arg) to use
 REM c0 = color for sequence term label "0"
 REM c1 = color for sequence term label "1"
-%~dp0\..\bin\remote.exe 127.0.0.1 7000 fixstim -a -b gray -d 813 -e 1010101010101010101010101010101010101010101010101010101010101010 -L 15,0,64,(0/0/0),(255/255/255)
+d:\work\usrig\bin\remote.exe 127.0.0.1 7000 fixstim -a -b gray -d 813 -e 1010101010101010101010101010101010101010101010101010101010101010 -L 30,0,64,(127/127/127),(255/255/255)
